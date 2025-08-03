3d4f4d2391a572a5921a9b5d6bf9ec68a0825722
Luiz Henrique Z. Steinbach <henrique-steinbach@hotmail.com>
2025-08-03 17:12:19 -0300

    Cria teste de cadastro sucesso para clínica; Verifica se /cadastro redireciona corretamente; Adiciona verificação de visibilidade de elementos; Verifica se a URL é /login após cadastro; Adiciona verificação de visibilidade de texto após cadastro;'

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
          setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseurl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:3000',
     video: true,
       reporter: 'mochawesome',
       reporterOptions: {
